55c3148bcce2aa93fa7fe98c310f339e6db75efb
wilfred jerome castelino <wc335@afsaccess1.njit.edu>
2017-11-20 21:51:07 -0500

    Added Html Table Class

---
+++ htmlTable.php
@@ -0,0 +1,31 @@
+<?php
+
+class htmlTable
+{
+
+    static public function displayAllRecords($tableRecords)
+    {
+        echo "<table  border=\"3\">";
+
+        echo "<tr>
+                <th>ID</th>
+                <th>OwnerEmail</th>
+                <th>OwnerID</th>
+                <th>Create Date</th>
+                <th>Due Date</th>
+                <th>Message</th>
+                <th>ISDone</th>
+        </tr>";
+
+        foreach($tableRecords as $key=>$row) {
+            echo "<tr>";
+            foreach($row as $key2=>$row2){
+                echo "<td>" . $row2 . "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+}
+
+?>
